ab8b6feec0927f84e1598a9be197b5b19f654299
Dmitriy Kuts <me@exileed.com>
2021-02-01 21:33:54 +0100

    Added scope payment.callbacks:write

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -44,7 +44,7 @@ import axios from 'axios'
 					grant_type: "password" ,
 					username: this.login,
 					password: btoa(this.password),
-					scope: "casino:read bonus:read bonus.settings:read bonus:write lab:read lab:write game:read game:write game.history:read game.wallet:write game.launch:write player:read player:write message:read message:write payment:read payment:write player:write:all message:write winner:read faq:read news:read slider:read"
+					scope: "casino:read bonus:read bonus.settings:read bonus:write lab:read lab:write game:read game:write game.history:read game.wallet:write game.launch:write player:read player:write message:read message:write payment:read payment:write player:write:all message:write winner:read faq:read news:read slider:read payment.callbacks:write"
 
 				}
 				console.log(userLog)
