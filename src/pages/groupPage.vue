d309495ff644c2e6bf7cd35c61d9660df5317dfa
Gevorg Hovsepyan <gevorghovsepyan95@gmail.com>
2021-02-01 23:22:12 +0100

    implementation of pager functionality

--- src/pages/groupPage.vue
+++ src/pages/groupPage.vue
@@ -11,12 +11,18 @@
 					<div class="text-center" v-if="loader">
 						<img src="../assets/img/icons/nv6.svg" class="spin">
 					</div>
+
 					<div class="text-center" v-else-if="gamesArr < 1">
 						<h3>Oops, No games yet!</h3>
 					</div>
-
 					<gameBox v-for="game in gamesArr" :game="game" v-else />
 				</div>
+
+        <ul id="pager">
+          <li v-for="(pagerElem, index) in pagerItems()">
+            <a @click="changePage(pagerElem.pageNumber)">{{pagerElem.title}}</a>
+          </li>
+        </ul>
 
 			</div>
 		</div>
@@ -30,37 +36,64 @@
 import axios from 'axios'
 import Navbar from '../components/ui/Navbar.vue'
 import gameBox from '../components/ui/gameBox.vue'
-
+  const API_GAMES_ENDPOINT = 'http://api.casinoplatform.site/v3/games';
+  const API_GAMES_DEFAULT_FIELDS = 'details,launch_types,images,type,provider,canonical';
 	export default{
 		components: { gameBox, Navbar },
 		props: ["id"],
 		data(){
 			return{
 				gamesArr: [],
+        pageCount: 0,
+        currentPage: 1,
 				loader: true
 			}
 		},
-		created(){
-			console.log(this.$route.params.id)
+		created() {
 			let link = this.$route.params.id.toString()
 			axios
       		.get('http://api.casinoplatform.site/v3/games?expand=details,launch_types,images,type,provider,canonical&group_id=' + link)
-      		.then(res => {
-         		this.gamesArr = res.data
-         		this.loader = false
-       		})
+      		.then(this._resCallback.bind(this))
 		},
 		methods: {
+		  changePage(pageNumber) {
+        this.loader = true
+        let link = this.$route.params.id.toString()
+        axios.get(API_GAMES_ENDPOINT, {
+          params: {
+            expand: API_GAMES_DEFAULT_FIELDS,
+            group_id: link,
+            page: pageNumber
+          }
+        }).then(this._resCallback.bind(this))
+      },
+		  pagerItems() {
+		    let pagerItems = [];
+		    for (let i = 0; i < this.pageCount; i++) {
+		      let pageNum = i + 1;
+		      pagerItems.push({
+            title: pageNum,
+            pageNumber: pageNum
+          });
+        }
+        return pagerItems
+      },
 			updateDynPage(){
 				this.loader = true
 				let link = this.$route.params.id.toString()
-				axios
-	      		.get('http://api.casinoplatform.site/v3/games?expand=details,launch_types,images,type,provider,canonical&group_id=' + link)
-	      		.then(res => {
-	         		this.gamesArr = res.data
-	         		this.loader = false
-	       		})
-			}
+        axios.get(API_GAMES_ENDPOINT, {
+          params: {
+            expand: API_GAMES_DEFAULT_FIELDS,
+            group_id: link
+          }
+        }).then(this._resCallback.bind(this))
+			},
+      _resCallback(res) {
+        this.currentPage = res.headers['x-pagination-current-page']
+        this.pageCount = res.headers['x-pagination-page-count']
+        this.gamesArr = res.data
+        this.loader = false
+      },
 		}
 	}
 </script>
