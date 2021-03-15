1e1d7f6bd360307c5683c133a64c73c2281a3952
Gevorg Hovsepyan <gevorghovsepyan95@gmail.com>
2021-03-15 21:12:22 +0100

    remove prevend rendering

--- src/components/mainSlider.vue
+++ src/components/mainSlider.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="mainSlider">
     <div class="container">
-      <div class="row slider-row" v-if="imagesLoaded">
+      <div class="row slider-row">
         <img src="../assets/img/blurcoins.svg" class="blur-coins hidden-xs hidden-sm hidden-md">
         <img src="../assets/img/smallbone.svg" class="small-bone hidden-xs hidden-sm hidden-md">
         <img src="../assets/img/blur-pastic.svg" class="blur-plastic hidden-xs hidden-sm hidden-md">
