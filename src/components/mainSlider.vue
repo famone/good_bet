e0eb444eee6c0cc5232aa51a35a21b35c3853940
Gevorg Hovsepyan <gevorghovsepyan95@gmail.com>
2021-03-15 19:39:36 +0100

    fix cs

--- src/components/mainSlider.vue
+++ src/components/mainSlider.vue
@@ -1,10 +1,10 @@
 <template>
-	<section id="mainSlider">
-		<div class="container">
-			<div class="row slider-row">
-				<img src="../assets/img/blurcoins.svg" class="blur-coins hidden-xs hidden-sm hidden-md" alt="">
-				<img src="../assets/img/smallbone.svg" class="small-bone hidden-xs hidden-sm hidden-md" alt="">
-				<img src="../assets/img/blur-pastic.svg" class="blur-plastic hidden-xs hidden-sm hidden-md" alt="">
+  <section id="mainSlider">
+    <div class="container">
+      <div class="row slider-row">
+        <img src="../assets/img/blurcoins.svg" class="blur-coins hidden-xs hidden-sm hidden-md" alt="">
+        <img src="../assets/img/smallbone.svg" class="small-bone hidden-xs hidden-sm hidden-md" alt="">
+        <img src="../assets/img/blur-pastic.svg" class="blur-plastic hidden-xs hidden-sm hidden-md" alt="">
 
         <swiper ref="mySwiper" :options="swiperOptions">
           <swiper-slide v-for="item in slider" v-bind:key="item.id">
@@ -30,40 +30,40 @@
 <script>
 import {mapGetters} from 'vuex'
 
-	export default{
-		data(){
-			return{
-				swiperOptions: {
-                  slidesPerView: 1,
-                  spaceBetween:30,
-                  loop: true,
-                  touchRatio: 1,
-                  infinite: true,
-                   effect: 'slide',
-                  speed: 800,
-                  centeredSlides: false,
-                  pagination: {
-			        el: '.swiper-pagination',
-			        clickable: true,
-			      },
-			      autoplay: {
-			        delay: 2500,
-			        disableOnInteraction: false,
-			      },
-			      navigation: {
-			        nextEl: '.button-next',
-			        prevEl: '.button-prev',
-			      },
-            	}
-			}
-		},
-		computed: {
-	    	...mapGetters({slider: "slider/getAll"}),
-		},
-		created() {
-      this.$store.dispatch("slider/loadAll");
+export default {
+  data() {
+    return {
+      swiperOptions: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        touchRatio: 1,
+        infinite: true,
+        effect: 'slide',
+        speed: 800,
+        centeredSlides: false,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: '.button-next',
+          prevEl: '.button-prev',
+        },
+      }
     }
+  },
+  computed: {
+    ...mapGetters({slider: "slider/getAll"}),
+  },
+  created() {
+    this.$store.dispatch("slider/loadAll");
   }
+}
 </script>
 
 <style>
